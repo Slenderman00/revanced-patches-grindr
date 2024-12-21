1809421e2bff9d73a56f5fbfeb386926c12659c3
Joar Heimonen <contact@joar.me>
2024-12-21 19:02:54 +0100

    feat: updated version

--- src/main/kotlin/grindr/patches/grindr/version/patch/spoofVersion.kt
+++ src/main/kotlin/grindr/patches/grindr/version/patch/spoofVersion.kt
@@ -40,7 +40,7 @@ class SpoofVersionPatch : BytecodePatch(
         private var grindrVersion =
         stringPatchOption(
             key = "grindr-version",
-            default = "24.17.1",
+            default = "24.19.0",
             title = "Grindr version",
             description = "The grindr version to spoof.",
             required = true,
@@ -49,7 +49,7 @@ class SpoofVersionPatch : BytecodePatch(
         private var grindrVersionIdentifier =
         stringPatchOption(
             key = "grindr-version-identifier",
-            default = "131488",
+            default = "132462",
             title = "Grindr version identifier",
             description = "The grindr version identifier to spoof.",
             required = true,
